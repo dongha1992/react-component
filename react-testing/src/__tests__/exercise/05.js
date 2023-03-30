3056e2d4bf99117476d468984b00c8ede9498267
dongha1992 <kamdunga2002@gmail.com>
2023-03-30 19:20:34 +0900

    geolocation test

--- react-testing/src/__tests__/exercise/05.js
+++ react-testing/src/__tests__/exercise/05.js
@@ -1,48 +1,94 @@
 // mocking HTTP requests
 // http://localhost:3000/login-submission
 
-import * as React from 'react'
-// 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {build, fake} from '@jackfranklin/test-data-bot'
-// 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
-import Login from '../../components/login-submission'
+import * as React from "react";
+
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { build, fake } from "@jackfranklin/test-data-bot";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import Login from "../../components/login-submission";
 
 const buildLoginForm = build({
   fields: {
-    username: fake(f => f.internet.userName()),
-    password: fake(f => f.internet.password()),
+    username: fake((f) => f.internet.userName()),
+    password: fake((f) => f.internet.password()),
   },
-})
+});
 
-// 🐨 get the server setup with an async function to handle the login POST request:
-// 💰 here's something to get you started
-// rest.post(
-//   'https://auth-provider.example.com/api/login',
-//   async (req, res, ctx) => {},
-// )
-// you'll want to respond with an JSON object that has the username.
-// 📜 https://mswjs.io/
+const server = setupServer(
+  rest.post(
+    "https://auth-provider.example.com/api/login",
+    async (req, res, ctx) => {
+      if (!req.body.password) {
+        return res(
+          ctx.status(400),
+          ctx.json({ message: "password is strongly required" })
+        );
+      }
+      if (!req.body.username) {
+        return res(
+          ctx.status(400),
+          ctx.json({ message: "username required " })
+        );
+      }
+      return res(ctx.json({ username: req.body.username }));
+    }
+  )
+);
 
-// 🐨 before all the tests, start the server with `server.listen()`
-// 🐨 after all the tests, stop the server with `server.close()`
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
 
-test(`logging in displays the user's username`, async () => {
-  render(<Login />)
-  const {username, password} = buildLoginForm()
+test(`알 수 없는 서버 에러 발생한 경우`, async () => {
+  const testErrorMessage = "unknown error";
+  server.use(
+    rest.post(
+      "https://auth-provider.example.com/api/login",
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({ message: testErrorMessage }));
+      }
+    )
+  );
 
-  await userEvent.type(screen.getByLabelText(/username/i), username)
-  await userEvent.type(screen.getByLabelText(/password/i), password)
-  // 🐨 uncomment this and you'll start making the request!
-  // await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  render(<Login />);
 
-  // as soon as the user hits submit, we render a spinner to the screen. That
-  // spinner has an aria-label of "loading" for accessibility purposes, so
-  // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
-  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
+  await userEvent.click(screen.getByRole("button", { name: /submit/i }));
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
 
-  // once the login is successful, then the loading spinner disappears and
-  // we render the username.
-  // 🐨 assert that the username is on the screen
-})
+  expect(screen.getByRole("alert")).toHaveTextContent(testErrorMessage);
+});
+
+test(`로그인 시 유저 이름이 화면에 나와야 함`, async () => {
+  render(<Login />);
+  const { username, password } = buildLoginForm();
+
+  await userEvent.type(screen.getByLabelText(/username/i), username);
+  await userEvent.type(screen.getByLabelText(/password/i), password);
+  await userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
+
+  expect(screen.getByText(username)).toBeInTheDocument();
+});
+
+test(`로그인 시 비밀번호를 입력하지 않아 에러가 발생한 경우`, async () => {
+  render(<Login />);
+  const { username } = buildLoginForm();
+
+  await userEvent.type(screen.getByLabelText(/username/i), username);
+  // 비밀번호 userEvent 생략
+  await userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
+
+  expect(screen.getByRole("alert").textContent).toMatchInlineSnapshot(
+    `"password is strongly required"`
+  );
+});
